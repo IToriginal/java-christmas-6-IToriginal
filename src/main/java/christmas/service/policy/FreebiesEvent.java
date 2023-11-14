bd634e00ce2b94a99ab4d8eba0f503b6a751a933
IToriginal <gitshineit@gmail.com>
2023-11-14 14:59:40 +0900

    :recycle: [refactor] #2 FreebiesEvent 메서드 분리 및 매직넘버 상수화

--- src/main/java/christmas/service/policy/FreebiesEvent.java
+++ src/main/java/christmas/service/policy/FreebiesEvent.java
@@ -2,6 +2,8 @@ package christmas.service.policy;
 
 import static christmas.util.content.InformationMessage.FREEBIES_EVENT;
 import static christmas.util.rule.PlannerConstant.FREEBIES_TARGET;
+import static christmas.util.rule.PlannerConstant.ZERO_VALUE;
+import static christmas.util.rule.RestaurantMenu.DRINK_CHAMPAGNE;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -24,15 +26,22 @@ public class FreebiesEvent implements Discount {
     @Override
     public Integer calculateDiscount(Integer totalOrderAmount, LocalDate reservationDate,
             HashMap<String, Integer> menuCount) {
-        // 이벤트 기간: 2023.12.01(금) ~ 2023.12.31(일)
-        if (reservationDate.isBefore(startDate) || reservationDate.isAfter(endDate)) {
-            return 0;
+        if (isDiscountableDate(reservationDate)) {
+            return ZERO_VALUE.getValue();
         }
 
-        if (totalOrderAmount >= FREEBIES_TARGET.getValue()) {
-            return 25000;
+        if (isFreebiesTarget(totalOrderAmount)) {
+            return DRINK_CHAMPAGNE.getPrice();
         }
-        return 0;
+        return ZERO_VALUE.getValue();
+    }
+
+    private boolean isDiscountableDate(LocalDate reservationDate) {
+        return reservationDate.isBefore(startDate) || reservationDate.isAfter(endDate);
+    }
+
+    private boolean isFreebiesTarget(Integer totalOrderAmount) {
+        return totalOrderAmount >= FREEBIES_TARGET.getValue();
     }
 
 }
