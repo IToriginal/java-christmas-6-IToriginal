d64a4dd998531041d98bcf6337577208a0797399
IToriginal <gitshineit@gmail.com>
2023-11-15 15:44:03 +0900

    :sparkles: [feature] #2 메모리 저장된 값을 조회할 때, Null값이 조회될 경우 예외 처리 기능 구현

--- src/main/java/christmas/repository/MemoryPlannerRepository.java
+++ src/main/java/christmas/repository/MemoryPlannerRepository.java
@@ -1,10 +1,15 @@
 package christmas.repository;
 
+import static christmas.util.content.ErrorMessage.ERROR_WORD;
+import static christmas.util.content.ErrorMessage.REPO_MENU_ERROR;
+import static christmas.util.content.ErrorMessage.REPO_RESERVATION_ERROR;
+
 import christmas.domain.Planner;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class MemoryPlannerRepository implements PlannerRepository {
@@ -20,42 +25,60 @@ public class MemoryPlannerRepository implements PlannerRepository {
     public Optional<LocalDate> findReservationDate() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getReservationDate);
+                .map(Planner::getReservationDate)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_RESERVATION_ERROR.getContent()));
     }
 
     @Override
     public Optional<HashMap<String, Integer>> findMenu() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getMenu);
+                .map(Planner::getMenu)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_MENU_ERROR.getContent()));
     }
 
     @Override
     public Optional<Integer> findTotalOrderAmount() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getTotalOrderAmount);
+                .map(Planner::getTotalOrderAmount)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_MENU_ERROR.getContent()));
     }
 
     @Override
     public Optional<HashMap<String, Integer>> findBenefits() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getBenefits);
+                .map(Planner::getBenefits)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_MENU_ERROR.getContent()));
     }
 
     @Override
     public Optional<Integer> findBenefitsAmount() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getBenefitsAmount);
+                .map(Planner::getBenefitsAmount)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_MENU_ERROR.getContent()));
     }
 
     @Override
     public Optional<String> findEventBadge() {
         return memory.stream()
                 .findFirst()
-                .map(Planner::getEventBadge);
+                .map(Planner::getEventBadge)
+                .map(Optional::ofNullable)
+                .orElseThrow(() -> new NoSuchElementException(
+                        ERROR_WORD.getContent() + REPO_MENU_ERROR.getContent()));
     }
 
     @Override
--- src/main/java/christmas/util/content/ErrorMessage.java
+++ src/main/java/christmas/util/content/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
 
     ERROR_WORD("[ERROR] "),
     RESERVATION_DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    FORMAT_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    FORMAT_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    REPO_RESERVATION_ERROR("예약된 날짜가 없습니다."),
+    REPO_MENU_ERROR("예약된 메뉴가 없습니다.");
 
     private final String content;
 
